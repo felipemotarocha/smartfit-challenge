a5ebff613d943d5858f95115599a5ab37a85a8b6
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-08 20:32:15 -0300

    feat: add Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-black flex items-center justify-center p-5">
+      <img width={180} src="/logo.svg" />
+    </div>
+  );
+};
+
+export default Header;
