99843bd6df18e8f33e7234d60a6ee2f7fcfb5155
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-08 20:32:28 -0300

    feat: add Welcome component

---
+++ src/components/welcome.tsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <div className="py-12 px-5">
+      <h1 className="text-4xl font-bold uppercase mb-6">
+        Reabertura
+        <br />
+        SmartFit
+      </h1>
+
+      <div className="h-4 w-36 bg-brand-darkGrey mb-8"></div>
+
+      <p>
+        O horário de funcionamento das nossas unidades está seguindo os decretos
+        de cada município. Por isso, confira aqui se a sua unidade está aberta e
+        as medidas de segurança que estamos seguindo.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
