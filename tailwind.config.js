604f367fa90f9bb83423290d8783af84436fd8bd
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-08 20:31:34 -0300

    chore: add brand colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          darkGrey: "#333333",
+          lightGrey: "#808080",
+          yellow: "#FFB612",
+          red: "#dc0a17",
+          green: "#2FC022",
+        },
+      },
+    },
   },
   plugins: [],
 };
